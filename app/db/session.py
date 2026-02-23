05562188cb1f82b71f93414afc337b836b2542f9
Ayush Kumar <ayush438500@gmail.com>
2026-02-23 12:32:06 +0530

    Create Session Maker and engine

---
+++ app/db/session.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from app.config import settings
+
+engine = create_async_engine(
+    url=settings.db_url,
+    echo = False,
+)
+
+sessions = async_sessionmaker(
+    bind= engine,
+    expire_on_commit= False,
+    class_= AsyncSession,
+)
+
