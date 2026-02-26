e008c26b61ba3b161ba5187d34d40c351c0b47aa
Ayush Kumar <ayush438500@gmail.com>
2026-02-26 21:17:08 +0530

    logging configuration

---
+++ app/helpers/logging.py
@@ -0,0 +1,33 @@
+import logging
+import sys
+import structlog
+
+
+def setup_logging(debug: bool = False) -> None:
+    log_level = logging.DEBUG if debug else logging.INFO
+
+    logging.basicConfig(
+        level=log_level,
+        format="[%(asctime)s] [%(levelname)s] %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        stream=sys.stdout,
+    )
+
+    if debug:
+        renderer = structlog.dev.ConsoleRenderer()
+    else:
+        renderer = structlog.processors.JSONRenderer()
+
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso", utc=True),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            renderer,
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(log_level),
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
