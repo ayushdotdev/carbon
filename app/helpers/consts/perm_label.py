538a746ab898f32cb16ea58185107624d9bdfaaa
Ayush Kumar <ayush438500@gmail.com>
2026-03-04 17:42:11 +0530

    Permission labels

---
+++ app/helpers/consts/perm_label.py
@@ -0,0 +1,11 @@
+from app.i18n.marker import _
+
+PERMISSION_LABELS = {
+    "manage_messages": _("Manage Messages"),
+    "ban_members": _("Ban Members"),
+    "kick_members": _("Kick Members"),
+    "moderate_members": _("Timeout Members"),
+    "manage_roles": _("Manage Roles"),
+    "manage_channels": _("Manage Channels"),
+    "administrator": _("Administrator"),
+}
