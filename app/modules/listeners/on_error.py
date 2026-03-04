f257de19e472660e1289b43ae43beb85cb696b2b
Ayush Kumar <ayush438500@gmail.com>
2026-03-04 17:47:10 +0530

    on_error listener completed

--- app/modules/listeners/on_error.py
+++ app/modules/listeners/on_error.py
@@ -2,11 +2,13 @@ import discord
 from discord.ext import commands
 
 from app.bot import Carbon
+from app.services.listeners.on_error import ErrorService
 
 
 class ErrorCog(commands.Cog):
     def __init__(self, bot: Carbon) -> None:
         self.bot = bot
+        self.service = ErrorService(self.bot)
 
     @commands.Cog.listener()
     async def on_app_command_error(
@@ -14,4 +16,7 @@ class ErrorCog(commands.Cog):
         interaction: discord.Interaction,
         error: discord.app_commands.AppCommandError,
     ) -> None:
-        pass
+        result = await self.service._on_error(interaction, error)
+
+        if result is not None:
+            await interaction.response.send_message(embed=result, ephemeral=True)
