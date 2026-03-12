4fe7cbde0e5cf5e2fe340e4a937a3825f248b354
Ayush Kumar <ayush438500@gmail.com>
2026-03-12 20:28:59 +0530

    Action on member embed

--- app/ui/embeds/log_embeds.py
+++ app/ui/embeds/log_embeds.py
@@ -1,7 +1,9 @@
+import discord
 from app.utils.confs.enums import ModLogAction
 from app.utils.core.embed import Embed
 from app.utils.helpers.embed_factory import EmbedFactory
 from app.i18n.manager import I18nManager
+from app.i18n.marker import _
 
 
 class LogEmbed(EmbedFactory):
@@ -11,4 +13,25 @@ class LogEmbed(EmbedFactory):
     def base_log_embed(self, action: ModLogAction) -> Embed:
         embed = self._build()
         embed.set_title_i18n(action.title)
+        return embed
+
+    def action_on_user(
+        self,
+        action: ModLogAction,
+        target: discord.Member,
+        moderator: discord.Member,
+        reason: str,
+        duration: int | str = "Permanent",
+    ) -> Embed:
+        embed = self.base_log_embed(action).set_description_i18n(
+            _(
+                "**User:** %(target_name)s ( %(target_id)s )\n> **Moderator:** %(mod_name)s ( %(mod_id)s )\n> **Reason:** %(reason)s\n> **Duration:** %(duration)s"
+            ),
+            target_name=target.mention,
+            target_id=target.id,
+            mod_name=moderator.mention,
+            mod_id=moderator.id,
+            reason=reason,
+            duration=duration,
+        )
         return embed
